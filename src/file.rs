72a89ecac5df5a409de1ee512ba71102d7ef6aa6
amigin <amigin@gmail.com>
2025-01-02 12:42:44 +0200

    Now Backup is created in iCloud

--- src/file.rs
+++ src/file.rs
@@ -63,24 +63,25 @@ pub fn save_current_as_backup(file_name: &str) {
 
     match read_file_content {
         Ok(content) => {
-            let folder_to_create = compile_full_filename("backups");
+            //let folder_to_create = compile_full_filename("backups");
 
-            if !std::path::Path::new(folder_to_create.as_str()).exists() {
-                fs::create_dir(folder_to_create.as_str()).unwrap();
+            let mut icloud_docs_path = format!(
+                "{}/Library/Mobile Documents/com~apple~CloudDocs/backups/my-secrets",
+                std::env::var("HOME").unwrap()
+            );
+
+            if !std::path::Path::new(icloud_docs_path.as_str()).exists() {
+                fs::create_dir(icloud_docs_path.as_str()).unwrap();
             }
 
-            let backup_file_name = compile_full_filename(
-                format!(
-                    "backups/{}.bak",
-                    &DateTimeAsMicroseconds::now().to_rfc3339()[..19],
-                )
-                .as_str(),
-            )
-            .replace(":", "-");
-            let res = fs::write(backup_file_name.as_str(), content);
+            icloud_docs_path.push_str("/");
+
+            icloud_docs_path
+                .push_str(&DateTimeAsMicroseconds::now().to_rfc3339().replace(":", "-")[..19]);
+            let res = fs::write(icloud_docs_path.as_str(), content);
 
             if let Err(err) = res {
-                println!("Can not write file: {}. Err: {}", backup_file_name, err);
+                println!("Can not write file: {}. Err: {}", icloud_docs_path, err);
             }
         }
         Err(err) => {
